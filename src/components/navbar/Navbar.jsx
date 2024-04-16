c209400ec86aa7e1822d61860859be6cb98a7a88
Jaerker <johanjakberger@gmail.com>
2024-04-16 08:19:43 +0200

    fixed bug in navbar button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,24 +3,29 @@ import Logo from '../../assets/logo.svg';
 import './navbar.css'
 
 function Navbar() {
-  return (
-    <nav className='navigation-field'>
-      <img src={Logo} alt='Företagslogga, bild på en uggla.' />
-      <ul className='navigation-field__nav-list'>
-        <li className='navigation-field__nav-item'>
-          <Link to='/' className=''>Home</Link>
-        </li>
-        <li className='navigation-field__nav-item'>
-          <Link to='/favourites' className=''>Favourites</Link>
-        </li>
-        <li className='navigation-field__nav-item'>
-          <Link to='/watchlist' className=''>Watchlist</Link>
-        </li>
+	return (
+		<nav className='navigation-field'>
+			<img src={Logo} alt='Företagslogga, bild på en uggla.' />
+			<ul className='navigation-field__nav-list'>
+				<Link to='/'>
+					<li className='navigation-field__nav-item'>
+						Home
+					</li>
+				</Link>
+				<Link to='/favourites'>
+					<li className='navigation-field__nav-item'>
+						Favourites
+					</li>
+				</Link>
+				<Link to='/watchlist'>
+					<li className='navigation-field__nav-item'>
+						Watchlist
+					</li>
+				</Link>
+			</ul>
+		</nav>
 
-      </ul>
-    </nav>
-
-  );
+	);
 }
 
 export default Navbar;
