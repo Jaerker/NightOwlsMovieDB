a17783004afe1b7f3b2475616066c391fabd7439
divperedi <132815746+divperedi@users.noreply.github.com>
2024-04-17 11:27:39 +0200

    changes in layout, added backdrop img

--- src/components/movieItem/MovieItem.jsx
+++ src/components/movieItem/MovieItem.jsx
@@ -3,9 +3,9 @@ import agent from '../../api/agent';
 import { useState, useEffect } from 'react';
 
 function MovieItem({ id }) {
-    console.log(id)
     const [movieDetails, setMovieDetails] = useState(null);
     const [trailerUrl, setTrailerUrl] = useState(null);
+    const [loading, setLoading] = useState(true); // Add this line
 
     useEffect(() => {
         const fetchMovieDetails = async () => {
@@ -23,6 +23,9 @@ function MovieItem({ id }) {
                             setTrailerUrl(trailer);
                         }
                     }
+                    // Lägger till setTimeout för att slippa ha 
+                    // "Could not find the movie" medans sida laddas
+                    setTimeout(() => setLoading(false), 50);
                 } catch (err) {
                     console.error(err);
                 }
@@ -32,12 +35,49 @@ function MovieItem({ id }) {
         fetchMovieDetails();
     }, [id]);
 
+    // setTimeout hjälper inte varje gång, därför har vi "Loading",
+    // annars visar det "Could not find movie" när sidan laddas om
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     if (!movieDetails) {
         return <div>Could not find the movie...</div>;
     }
 
     return (
         <div className="movie-container">
+            <div className="single-movie-item" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url(https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path})` }}>
+            <div className="movie-poster" >
+                    {/* Kollar om det finns poster,
+                    renderar "No poster was found" text ifall det saknas */}
+                    {movieDetails.poster_path ? (
+                        <img src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} 
+                            alt={`${movieDetails.original_title} movie poster`} 
+                            className="movie-poster" />
+                    ) : (
+                        <p className="no-movie-msg">No poster was found</p>
+                    )}
+                </div>
+                <div className="movie-info">
+                    <h1>{movieDetails.original_title}</h1>
+                    {/* Visar tagline bara om det finns */}
+                    {movieDetails.tagline && <h2>{movieDetails.tagline}</h2>}
+                    <p>Runtime: {movieDetails.runtime} minutes</p>
+                    <p>Rating: {movieDetails.vote_average}</p>
+                    <p>Genre: {movieDetails.genres 
+                                && movieDetails.genres.map((genre) => genre.name).join(', ')}</p>
+                    {/* Visar homepage länk bara om det finns */}
+                    {movieDetails.homepage && <a href={movieDetails.homepage} 
+                                                className="movie-info-link">Visit Homepage</a>}
+                    <p>{movieDetails.overview}</p>
+                    <div className="single-movie-buttons">
+                        <button className="movie-button">Add to Favourites</button>
+                        <button className="movie-button">Add to Watchlist</button>
+                    </div>
+                </div>
+            </div>
+            {/* Visar trailer div bara om trailer finns */}
             {trailerUrl && (
                 <div className="trailer">
                     <iframe
@@ -56,28 +96,6 @@ function MovieItem({ id }) {
                     ></iframe>
                 </div>
             )}
-            <div className="single-movie-item" >
-                <div className="movie-poster">
-                    <img src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} 
-                    alt={`${movieDetails.original_title} movie poster`} 
-                    className="movie-poster" />
-                </div>
-                <div className="movie-info">
-                    <h1>{movieDetails.original_title}</h1>
-                    {movieDetails.tagline && <h2>{movieDetails.tagline}</h2>}
-                    <p>Runtime: {movieDetails.runtime} minutes</p>
-                    <p>Rating: {movieDetails.vote_average}</p>
-                    <p>Genre: {movieDetails.genres 
-                                && movieDetails.genres.map((genre) => genre.name).join(', ')}</p>
-                    {movieDetails.homepage && <a href={movieDetails.homepage} 
-                                                className="movie-info-link">Visit Homepage</a>}
-                    <p>{movieDetails.overview}</p>
-                    <div className="single-movie-buttons">
-                        <button className="movie-button">Add to Favourites</button>
-                        <button className="movie-button">Add to Watchlist</button>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
