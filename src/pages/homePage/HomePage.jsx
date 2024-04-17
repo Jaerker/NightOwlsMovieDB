bfbb1e5b6673ff3a273a27e9666e945e056d9e00
Jaerker <johanjakberger@gmail.com>
2024-04-17 13:52:38 +0200

    cleaning up in homepage

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,37 +1,16 @@
 import { useEffect, useState } from "react";
 import agent from "../../api/agent";
 
-import Card from "../../components/card/Card";
-import CardContainer from "../../components/cardContainer/CardContainer";
 import './home-page.css';
+
 function HomePage() {
 
-	const [data, setData] = useState([]);
-
-	const getData = async () => {
-		const agentData = await agent.list.getPopular();
-		if(agentData !== null){
-			setData(agentData.results);
-		}
-	}
-	useEffect(() => {
-		getData();
-	}, []);
 
 	return (
 		<>
-				<CardContainer>
-					{data.length === 0 && <p className='loading-message'>Loading...</p>}
-					
-					{data.map((movie, index) => (
-						
-						<Card key={index} movie={movie} />
-					))}
-				</CardContainer>
+			Homepage
 		</>
 	);
 }
 
 export default HomePage;
-
-
