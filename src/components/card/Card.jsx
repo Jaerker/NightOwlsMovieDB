53fbe1de71c9b9054ada06f6aa384af615605136
Jaerker <johanjakberger@gmail.com>
2024-04-18 09:30:04 +0200

    saving before meeting

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,50 @@
+import { useNavigate } from 'react-router-dom';
+import { isInFavourites, isInWatchlist } from '../../controller/controller';
+import local from '../../api/local';
+
 import RoundButton from '../roundButton/RoundButton';
-import './card.css';
+
 import Star from '../../assets/star.svg';
 import Plus from '../../assets/plus.svg';
 import Minus from '../../assets/minus.svg';
-import { Link } from 'react-router-dom';
+import './card.css';
+
 
 function Card({ movie }) {
 
+	const navigate = useNavigate();
+
+	const cardClicked = (e) => {
+
+		switch(e.target.className){
+			case 'round-button round-button--favourites':
+				isInFavourites(movie.id) ? local.favourites.remove(movie).then(res => console.log(res)) : local.favourites.add(movie).then(res => console.log(res));
+				break;
+			case 'round-button round-button--watchlist':
+				isInWatchlist(movie.id) ? local.watchlist.remove(movie) : local.watchlist.add(movie);
+				break;
+			default:
+				navigate(`/movie/${movie.id}`);
+				break;
+		
+		}
+
+	}
+
 	const cardClass = { backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, .8) 100%), url(https://image.tmdb.org/t/p/w500${movie.poster_path})` };
 	return (
-		<Link to={`/movie/${movie.id}`} className='card' style={cardClass}>
+		<article onClick={(e) => cardClicked(e)} className='card' style={cardClass}>
 			<section className='card__top-right-buttons'>
-				<RoundButton className='round-button' content={<img src={Plus} alt='Plus icon' />} onClick={() => { }} />
-				<RoundButton className='round-button' content={<img src={Star} alt='Star icon' />} onClick={() => { }} />
+				<RoundButton className='round-button round-button--watchlist' content={
+				<>
+					<img className={`round-button__plus-icon ${isInWatchlist(movie.id) ? 'round-button__plus-icon--disabled' : 'round-button__plus-icon--enabled'}`} src={Plus} alt='Plus icon' />
+					<img className='round-button__minus-icon' src={Minus} alt='Minus icon' />
+				</>} onClick={() => { }} />
+				<RoundButton className='round-button round-button--favourites' content={<img className={`round-button__favourites ${isInFavourites(movie.id) ? 'round-button__favourites--true' : 'round-button__favourites--false'}`} src={Star} alt='Star icon' />} onClick={() => { }} />
 			</section>
 			<h3 className='movie-title'>{movie.title}</h3>
 			<RoundButton className='round-button round-button--rating' content={<h3 className='rating'>{Math.round(movie.vote_average * 10) / 10}</h3>} onClick={() => { }} />
-		</Link>
+		</article>
 	);
 }
 
--- src/pages/favouritesPage/FavouritesPage.jsx
+++ src/pages/favouritesPage/FavouritesPage.jsx
@@ -1,6 +1,33 @@
+import { useEffect } from 'react';
+import local from '../../api/local';
+import Card from '../../components/card/Card';
+import CardContainer from '../../components/cardContainer/CardContainer';
+import './favourites-page.css';
+import agent from '../../api/agent';
 function FavouritesPage() {
+	
+	
+	useEffect(() => {
+		getData();
+
+	}, []);
+	
+	const getData = async () => {
+		const data = await agent.list.getPopular().then(res => res.results);
+		console.log(data);
+		data.results.foreach(movie => local.favourites.add(movie));
+	}
+
 	return (
-		<div>FavouritesPage</div>
+	<>
+		<h1>Favourites</h1>
+		<CardContainer>
+		{local.favourites.get().map((movie, index) => (
+			<Card key={index} movie={movie} />	
+		))}		
+		</CardContainer>
+		
+	</>	
 	);
 }
 
