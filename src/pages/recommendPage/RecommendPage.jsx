dfd87b7755501ad6970ae91cdfaf1bbf2e18d069
divperedi <132815746+divperedi@users.noreply.github.com>
2024-04-18 16:02:57 +0200

    fixed rendering logic of RecommendPage, small css fixings

--- src/pages/recommendPage/RecommendPage.jsx
+++ src/pages/recommendPage/RecommendPage.jsx
@@ -5,38 +5,42 @@ import agent from '../../api/agent';
 import Card from '../../components/card/Card';
 
 function RecommendPage() {
-  const [results, setResults] = useState([]);
-  // Använder location för att rendera om sidan varje gång det besöks 
-  //(page reload eller tryck på Recommend knapp i navbar)
-  const location = useLocation();
-  // Key ändras varje gång sidan besöks, 
-  // när key ändras - görs de ny api anrop med random sida nummer i useEffect
-  const [key, setKey] = useState(location.key);
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(true);
+    // Använder location för att rendera om sidan varje gång det besöks 
+    //(page reload eller tryck på Recommend knapp i navbar)
+    const location = useLocation();
+    // Key ändras varje gång sidan besöks, 
+    // när key ändras - görs de ny api anrop med random sida nummer i useEffect
+    const [key, setKey] = useState(location.key);
 
-  const getTrending = async (page) => {
-      const data = await agent.list.getTrending(page);
-      setResults(data.results);
-  }
+    const getTrending = async (page) => {
+        const data = await agent.list.getTrending(page);
+        setResults(data.results);
+        setLoading(false);
+    }
 
-  // 
-  useEffect(() => {
-      const randomPage = Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 10
-      getTrending(randomPage);
-  }, [key]);
+    // 
+    useEffect(() => {
+        const randomPage = Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 10
+        getTrending(randomPage);
+    }, [key]);
 
-  useEffect(() => {
-      setKey(location.key);
-  }, [location]);
+    useEffect(() => {
+        setKey(location.key);
+    }, [location]);
 
-  return (
-      <>
-          <CardContainer>
-              {results.map((result, index) => (
-                  <Card key={index} movie={result} />
-              ))}
-          </CardContainer>
-      </>
-  );
+    return (
+        <>
+            {loading ? (
+                <p className="loading-message">Loading...</p>
+            ) : results.length > 0 ? (
+                <CardContainer list={results} />
+            ) : (
+                <p>No movies found</p>
+            )}
+        </>
+    );
 }
 
 export default RecommendPage;
