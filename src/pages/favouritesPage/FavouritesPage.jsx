51a337a046e87f6c2dadf2bc6633a2e153d31772
Jaerker <johanjakberger@gmail.com>
2024-04-23 10:41:41 +0200

    fixed favouritesPage.jsx

--- src/pages/favouritesPage/FavouritesPage.jsx
+++ src/pages/favouritesPage/FavouritesPage.jsx
@@ -1,14 +1,12 @@
 import CardContainer from '../../components/cardContainer/CardContainer';
 import './favourites-page.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import local from '../../api/local';
 
 function FavouritesPage() {
 	const [favourites, setFavourites] = useState(local.favourites.get());
-	useEffect(()=> {
-		setFavourites(local.favourites.get());
-	}, []);
+
  
 	return (
 	<>
